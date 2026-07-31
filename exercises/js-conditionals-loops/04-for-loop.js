f9c6a764d38b8ab5c38401497633180eec36e8f2
fon.suteara <fon.suteara@gmail.com>
2026-07-31 14:05:33 +0700

    Update Answer

--- exercises/js-conditionals-loops/04-for-loop.js
+++ exercises/js-conditionals-loops/04-for-loop.js
@@ -7,9 +7,14 @@
 // Use a classic for loop: for (let i = 1; i <= n; i++) { ... }
 
 function sumTo(n) {
-  // TODO: replace this with your implementation
-}
 
+  let total = 0;
+
+  for (let i = 1; i <= n; i++) {
+    total += i; 
+  }
+  return total;
+}
 // --- Tests (do not edit) ---
 import { strictEqual } from "node:assert";
 
